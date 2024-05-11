73f11622434211008f1f628e8b77fa5e7ce26631
Ridhima Mishra <100011986+mishri10@users.noreply.github.com>
2024-05-11 11:24:32 +0530

    Time: 43 ms (81.22%), Space: 48.1 MB (70.22%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -1,68 +1,81 @@
 class Node{
-    Node* links[26];
-    bool flag=0;
+    
+    
+   Node* links[26];
+   bool flag=false;
     
     public:
     
-    bool containsKey(char ch){
-        return (links[ch-'a']!=NULL);}
     
-    void put(char ch, Node* node){
-        links[ch-'a']=node;}
-    
-    Node* get(char ch){
-        return links[ch-'a'];}
-    
-    bool isEnd(){
-        return flag;}
-    
-    void setEnd(){
-        flag=1;
+    bool containsKey(char t){
+        return links[t-'a']!=NULL;
     }
     
+    void put(char t, Node* nxt){
+        links[t-'a']=nxt;
+    }
+    
+    Node* get(char t){
+        return links[t-'a'];
+    }
+    
+    void setEnd(){
+        flag=1;}
+    
+    bool getEnd(){
+        return flag;
+    }
+    
+    
+    
+    
 };
-
 class Trie {
-private:
-    Node* root;
 public:
+    Node* root;
     Trie() {
         root=new Node();
     }
     
     void insert(string word) {
-        Node* node=root;
         
-        for(int i=0;i<word.size();i++){
-            if(!node->containsKey(word[i])){
-                node->put(word[i],new Node());
+        Node* dummy=root;
+        
+        for(int i=0; i<word.size();i++){
+            if(!dummy->containsKey(word[i])){
+                dummy->put(word[i],new Node());
             }
-            node=node->get(word[i]);
+            
+            dummy=dummy->get(word[i]);
         }
         
-        node->setEnd();
-    }
+        dummy->setEnd();}
     
     bool search(string word) {
         
-        Node* node=root;
+        Node* dummy=root;
         
         for(int i=0;i<word.size();i++){
-            if(!node->containsKey(word[i]))return 0;
             
-            node=node->get(word[i]);
+            if(!dummy->containsKey(word[i]))return 0;
+            
+            else{
+                dummy=dummy->get(word[i]);
+            }
         }
-        return node->isEnd();
         
+        return dummy->getEnd();
     }
     
     bool startsWith(string prefix) {
         
-        Node* node=root;
+        Node* dummy=root;
+        
         for(int i=0;i<prefix.size();i++){
-            if(!node->containsKey(prefix[i]))return 0;
-            node=node->get(prefix[i]);
+            if(!dummy->containsKey(prefix[i]))return 0;
+            else dummy=dummy->get(prefix[i]);
         }
+        
         return 1;
         
     }
