e6d4bcfc5ae85faac0a0ded8b78adf3941470292
Ridhima Mishra <100011986+mishri10@users.noreply.github.com>
2024-05-12 10:38:39 +0530

    Time: 5 ms (34.30%), Space: 8.8 MB (72.93%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void helper(int ind,vector<int>& nums, vector<vector<int>>& ans){
+        
+        if(ind==nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        
+        
+        for(int i=ind;i<nums.size();i++){
+            swap(nums[i],nums[ind]);
+            helper(ind+1,nums,ans);
+            swap(nums[i],nums[ind]);
+            
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ans;
+        helper(0,nums,ans);        
+        return ans;
+        
+    }
+};
