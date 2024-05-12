eed3ef6ce64f6efcfd0eefe9fd8d4618d73c3711
Ridhima Mishra <100011986+mishri10@users.noreply.github.com>
2024-05-12 12:01:36 +0530

    Time: 12 ms (57.24%), Space: 7.8 MB (35.86%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-     
-    bool isValid(int row,int col,char c,vector<vector<char>>& board){
+  //  void helper
+        bool isValid(int row,int col,char c,vector<vector<char>>& board){
         
         for(int i=0; i<9;i++){
             
@@ -21,33 +21,33 @@ public:
     }
     
     bool solve(vector<vector<char>>& board){
+        
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
+                
                 if(board[i][j]=='.'){
                     
                     for(char ch='1';ch<='9';ch++){
                         
                         if(isValid(i,j,ch,board)){
+                            
                             board[i][j]=ch;
                             
                             if(solve(board))return 1;
                             
                             else board[i][j]='.';
                         }
-                       
-                        
                     }
                     return 0;
                 }
-               
-            }}
+            }
+        }
         return 1;
+        
     }
+    
+    
     void solveSudoku(vector<vector<char>>& board) {
-              
-        solve(board);
-        
-        
-        
+        solve(board);       
     }
 };
