63a3e73b53da315efc6e16a0e20424d058b76180
Ridhima Mishra <100011986+mishri10@users.noreply.github.com>
2024-05-16 11:59:45 +0530

    Time: 161 ms (9.42%), Space: 73.5 MB (37.91%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -10,70 +10,67 @@
  */
 class Solution {
 public:
-    
-    ListNode* findMid(ListNode* h){
-        ListNode* s=h, *f=h->next;
+    ListNode* findMid(ListNode* head){
+        ListNode* f=head->next;
+        ListNode* s=head;
         
         while(f and f->next){
             s=s->next;
             f=f->next->next;
         }
+        
         return s;
     }
-    
-    ListNode* merge(ListNode* l, ListNode* r){
+    ListNode* merge(ListNode* h1, ListNode* h2){
+        ListNode* dummy=new ListNode(1);
         
-        ListNode* dummy= new ListNode(1);
         ListNode* temp=dummy;
         
-        while(l and r){
-            if(l->val<=r->val){
-                temp->next=l;
-                l=l->next;
-               
+        while(h1 and h2){
+            if(h1->val<=h2->val){
+                temp->next=h1;
+                h1=h1->next;
             }
-            else{
-                temp->next=r;
-                r=r->next;
-                
+            else if(h2->val<h1->val){
+                temp->next=h2;
+                h2=h2->next;
             }
-             temp=temp->next;
-                
-        }
-        
-        while(l){
-            temp->next=l;
-            l=l->next;
             temp=temp->next;
         }
         
-        while(r){
-            temp->next=r;
-            r=r->next;
+        while(h1){
+            temp->next=h1;
+            h1=h1->next;
+            temp=temp->next;
+        }
+        
+        while(h2){
+            temp->next=h2;
+            h2=h2->next;
             temp=temp->next;
         }
         
         return dummy->next;
-    }
-    ListNode* ms(ListNode* head){
         
+    }
+    
+    
+    ListNode* mergeSort(ListNode* head){
         if(!head or !head->next)return head;
+        
         ListNode* mid=findMid(head);
-        ListNode* nx=mid->next;
+        
+        ListNode* h2=mid->next;
         mid->next=NULL;
+        ListNode* h1=head;
         
-        ListNode* left=head;
-        ListNode* right=nx;
+        h1=mergeSort(h1);
+        h2=mergeSort(h2);
         
-        left=ms(left);
-        right=ms(right);
-        
-        return merge(left,right);
-        
+        return merge(h1,h2);
     }
+    
     ListNode* sortList(ListNode* head) {
-       
-      
-       return ms(head);
+        return mergeSort(head);
     }
 };
