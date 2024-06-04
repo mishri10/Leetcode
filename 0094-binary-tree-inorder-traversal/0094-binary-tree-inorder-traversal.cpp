e9efc2c8b609cf27c829750359191be201f760f6
Ridhima Mishra <100011986+mishri10@users.noreply.github.com>
2024-06-04 13:48:55 +0530

    Time: 0 ms (100.00%), Space: 9.9 MB (40.64%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -11,16 +11,37 @@
  */
 class Solution {
 public:
-    void inorder(TreeNode* root, vector<int>& ans){
-        if(!root)return;
-        
-        inorder(root->left,ans);
-        ans.push_back(root->val);
-        inorder(root->right,ans);
-    }
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int>ans;
-        inorder(root,ans);
-        return ans;
+        //morris traversal
+        //left,root,right
+        
+        vector<int>inorder;
+        
+        TreeNode* cur=root;
+        while(cur){
+            if(!cur->left){
+                inorder.push_back(cur->val);
+                cur=cur->right;
+            }
+            
+            else{
+                TreeNode* prev=cur->left;
+                while(prev->right and prev->right!=cur){
+                    prev=prev->right;
+                }
+                
+                if(prev->right==NULL){
+                    prev->right=cur;
+                    cur=cur->left;
+                }
+                
+                else{
+                    prev->right=NULL;
+                    inorder.push_back(cur->val);
+                    cur=cur->right;
+                }
+            }
+        }
+        return inorder;
     }
 };
