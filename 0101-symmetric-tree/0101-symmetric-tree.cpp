db5a5c1ff20fac1d857165efd736efb39fc45bd9
Ridhima Mishra <100011986+mishri10@users.noreply.github.com>
2024-06-19 17:52:14 +0530

    Time: 3 ms (70.29%), Space: 17.4 MB (13.43%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -11,44 +11,14 @@
  */
 class Solution {
 public:
+    bool check(TreeNode* p, TreeNode* q){
+        if(p==NULL or q==NULL)return p==q;
+        
+        if(p->val!=q->val)return 0;
+        
+        return check(p->right,q->left) and check(p->left,q->right);
+    }
     bool isSymmetric(TreeNode* root) {
-        
-        stack<TreeNode*>st1,st2;
-        
-        st1.push(root->left);
-        st2.push(root->right);
-        
-        while(!st1.empty() and !st2.empty()){
-            TreeNode* node1=st1.top();
-            st1.pop();
-            TreeNode* node2=st2.top();
-            st2.pop();
-            
-            if(node1==NULL and node2==NULL)continue;
-            
-            if(node1==NULL and node2!=NULL)return 0;
-            if(node1!=NULL and node2==NULL)return 0;
-            
-            if(node1->val != node2->val)return 0;
-            
-            if(node1){
-                st1.push(node1->right);
-                st1.push(node1->left);}
-            if(node2){
-                st2.push(node2->left);
-                st2.push(node2->right);}
-        }
-        
-        return 1;
+        return check(root->left, root->right);
     }
 };
-
-// base case
-// if(l ==NULL or r==NULL)return l==r
-// if(l->val!=r->val)return false;
-// recursive function
-// (l->right and r->left) and (l->left and r->right)
-//T.C: O(N), S.C: O(H)
-
-
-
