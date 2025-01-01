e76b4240d695be1c3a9be854774e4dc86d24a4f7
Ridhima Mishra <100011986+mishri10@users.noreply.github.com>
2025-01-01 17:58:16 +0530

    Time: 0 ms (100%), Space: 27.2 MB (18.8%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -2,27 +2,24 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n=height.size();
-        vector<int>prefix(n), suffix(n);
-        int maxiL=0, maxiR=0;
-        for(int i=0;i<n;i++){
-            if(height[i]>maxiL)maxiL=height[i];
-            prefix[i]=maxiL;
-        }
-        
-        for(int i=n-1;i>=0;i--){
-            if(height[i]>maxiR)maxiR=height[i];
-            suffix[i]=maxiR;
-        }
-        
-        
+        vector<int>ngeRight(n);
+        vector<int>ngeLeft(n);
         int ans=0;
-        for(int i=0;i<n;i++){
-            ans+=(min(prefix[i],suffix[i])-height[i]);
+        ngeLeft[0]=height[0];
+        ngeRight[n-1]=height[n-1];
+
+        for(int i=1; i<n; i++)
+            ngeLeft[i]=max(height[i],ngeLeft[i-1]);
+        
+        for(int i=n-2; i>=0; i--)
+            ngeRight[i]=max(height[i],ngeRight[i+1]);
+        
+
+        for(int i=0; i<n; i++){
+            int possibleHeight= min(ngeRight[i],ngeLeft[i]);
+            ans+= (possibleHeight-height[i]);
         }
+
         return ans;
-        
-        
-        
-        
     }
 };
