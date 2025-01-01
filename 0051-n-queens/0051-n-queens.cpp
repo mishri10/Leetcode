2bd1f806da4cf5ce29b3c066b9486d0f6e9caaa7
Ridhima Mishra <100011986+mishri10@users.noreply.github.com>
2025-01-01 16:36:15 +0530

    Time: 3 ms (65.07%), Space: 10.2 MB (68.45%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,68 +1,70 @@
 class Solution {
 public:
-    
-    bool safe(int row,int col,vector<string>& board, int n){
-        
-        int x=row, y=col;
-        
-        //left side        
-        while(col>=0){
-            if(board[row][col]=='Q')return 0;
-            col--;
+    bool noAttack(int row, int col, vector<string>& board, int n){
+        int x=row;
+        int y=col;
+
+        //horizontally left
+        while(y>=0){
+            if(board[x][y]=='Q')
+                return 0;
+            
+            y--;
+
         }
-        
-        //upper diagonal
-        
-   
-        col=y;
-        while(row>=0 and col>=0){
-            if(board[row][col]=='Q')return 0;
-            row--;
-            col--;
+
+        //upper left diagonal
+        y=col;
+        while(x>=0 and y>=0){
+            if(board[x][y]=='Q')
+                return 0;
+            x--;
+            y--;
         }
-        
-        //lower diagonal
-        
-        row=x;
-        col=y;
-        while(row<n and col>=0){
-            if(board[row][col]=='Q')return 0;
-            row++;
-            col--;
+
+        //lower left diagonal
+        x=row;
+        y=col;
+
+        while(x<n and y>=0){
+            if(board[x][y]=='Q')
+                return 0;
+            x++;
+            y--;
         }
-        
+
         return 1;
     }
-    
-    void place(int col,int n,vector<string>& board, vector<vector<string>>& ans){
-        
-        //base case
-        if(col==n){
-            ans.push_back(board);
-            return;
-        }
-        
-        
-        for(int row=0;row<n;row++){
-            
-            if(safe(row,col,board,n)){
-                board[row][col]='Q';
-                place(col+1,n,board,ans);
-                board[row][col]='.';
+
+
+    void findPossible(int col, int n, vector<string>& board, 
+                    vector<vector<string>>& ans){
+                
+                if(col==n){
+                    ans.push_back(board);
+                    return;
+                }
+
+                for(int row=0; row<n; row++){
+                    if(noAttack(row,col,board,n)){
+                        board[row][col]='Q';
+                        findPossible(col+1,n,board,ans);
+                        board[row][col]='.';
+                    }
+                }
             }
-        }
-        
-        return;
-        
-    }
+
+
     vector<vector<string>> solveNQueens(int n) {
-        vector<string>board(n);
-        for(int i=0;i<n;i++){
-            board[i]=string(n,'.');
-        }
+        vector<string>board;
         vector<vector<string>>ans;
-        
-        place(0,n,board,ans);
+        string temp= string(n,'.');
+
+        for(int i=0; i<n; i++){
+            board.push_back(temp);
+        }
+
+        findPossible(0,n,board,ans);
         return ans;
     }
 };
