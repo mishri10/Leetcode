c204dff3e5d41926141ad0d9805901c9c4f4ada6
Ridhima Mishra <100011986+mishri10@users.noreply.github.com>
2025-02-22 16:55:00 +0530

    Time: 0 ms (100%), Space: 23.6 MB (72.38%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -11,20 +11,19 @@
  */
 class Solution {
 public:
-    int postorder(TreeNode* node, int& ans){
+    int helper(TreeNode* node, int& dia){
         if(node==NULL)
             return 0;
 
-        int lh= postorder(node->left,ans);
-        int rh= postorder(node->right,ans);
-
-        ans=max(ans,lh+rh);
+        int lh=helper(node->left,dia);
+        int rh=helper(node->right,dia);
+        dia=max(dia,lh+rh);
 
         return 1+max(lh,rh);
     }
     int diameterOfBinaryTree(TreeNode* root) {
-        int ans=0;
-        postorder(root,ans);
-        return ans;
+        int dia=0;
+        helper(root,dia);
+        return dia;
     }
 };
