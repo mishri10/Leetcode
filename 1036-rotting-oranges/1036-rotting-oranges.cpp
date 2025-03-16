682e0d642957b351e9563364ecfe8d04c24bed13
Ridhima Mishra <100011986+mishri10@users.noreply.github.com>
2025-03-16 12:50:40 +0530

    Time: 0 ms (100%), Space: 16.5 MB (99.03%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,57 +1,49 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
+        int fresh=0;
         queue<pair<int,int>>q;
         int n=grid.size();
-        int m=grid[0].size();
-        int time=0;
-        int fresh=0;
-
-        int uRow[4]={-1,0,1,0};
-        int uCol[4]={0,1,0,-1};
-
-        for(int row=0; row<n; row++){
-            for(int col=0; col<m; col++){
-                if(grid[row][col]==2){
-                    q.push({row,col});
-                }
-
-                else if(grid[row][col]==1)
+        int m=grid[0].size(); 
+        int dr[4]={-1,0,1,0};
+        int dc[4]={0,1,0,-1};
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(grid[i][j]==1)
                     fresh++;
-                
+                if(grid[i][j]==2)
+                    q.push({i,j});
             }
         }
 
-        while(!q.empty()){
-            int size= q.size();
-            bool flag=0;
+        int t=0;
 
-            for(int i=0; i<size; i++){
+        while(!q.empty()){
+            int sz=q.size();
+            bool rott=0;
+            for(int i=0; i<sz; i++){
                 int row=q.front().first;
                 int col=q.front().second;
-
                 q.pop();
 
                 for(int j=0; j<4; j++){
-                    int newRow=row+uRow[j];
-                    int newCol=col+uCol[j];
-
-                    if(newRow>=0 and newCol>=0 and newCol<m and newRow<n and
-                        grid[newRow][newCol]==1){
-                            flag=1;
-                            grid[newRow][newCol]=2;
-                            q.push({newRow,newCol});
-                            fresh--;
-                        }
+                    int newRow= row+dr[j];
+                    int newCol= col+dc[j];
+                    if(newRow>=0 and newRow<n and newCol>=0 and newCol<m and
+                       grid[newRow][newCol]==1){
+                        rott=1;
+                        grid[newRow][newCol]=2;
+                        fresh--;
+                        q.push({newRow,newCol});
+                       }
                 }
-
-                
             }
-            if(flag)
-                time++;
+            if(rott)
+                t++;
         }
         if(fresh!=0)
             return -1;
-        return time;
+        return t;
     }
 };
