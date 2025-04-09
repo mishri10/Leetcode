af91b6c5bcd34d0a888c26838e8f5fd6c81d7c1f
Ridhima Mishra <100011986+mishri10@users.noreply.github.com>
2025-04-09 09:26:08 +0530

    Time: 0 ms (100%), Space: 16.8 MB (67.54%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,49 +1,55 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int fresh=0;
+        int t=0;
         queue<pair<int,int>>q;
+        int freshOrange=0;
+
         int n=grid.size();
-        int m=grid[0].size(); 
+        int m=grid[0].size();
         int dr[4]={-1,0,1,0};
         int dc[4]={0,1,0,-1};
-        
+
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(grid[i][j]==1)
-                    fresh++;
-                if(grid[i][j]==2)
-                    q.push({i,j});
+            if(grid[i][j]==2){
+                q.push({i,j});
             }
-        }
 
-        int t=0;
+            else if(grid[i][j]==1)
+                freshOrange++;
+        }}
 
         while(!q.empty()){
-            int sz=q.size();
-            bool rott=0;
+            int originalFresh=freshOrange;
+            int sz= q.size();
+            
             for(int i=0; i<sz; i++){
-                int row=q.front().first;
-                int col=q.front().second;
-                q.pop();
+            int row= q.front().first;
+            int col= q.front().second;
 
-                for(int j=0; j<4; j++){
-                    int newRow= row+dr[j];
-                    int newCol= col+dc[j];
-                    if(newRow>=0 and newRow<n and newCol>=0 and newCol<m and
-                       grid[newRow][newCol]==1){
-                        rott=1;
+            q.pop();
+
+            for(int j=0; j<4; j++){
+                int newRow= row+ dr[j];
+                int newCol= col+ dc[j];
+
+                if(newRow>=0 and newRow<n and newCol>=0 and newCol<m and 
+                    grid[newRow][newCol]==1){
                         grid[newRow][newCol]=2;
-                        fresh--;
                         q.push({newRow,newCol});
-                       }
-                }
+                        freshOrange--;
+                    }
             }
-            if(rott)
+
+            
+        }
+        if(freshOrange<originalFresh)
                 t++;
         }
-        if(fresh!=0)
+        if(freshOrange!=0)
             return -1;
         return t;
+
     }
 };
