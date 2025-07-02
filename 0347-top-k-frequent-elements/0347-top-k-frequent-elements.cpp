9e2ad70cef90a7cfbad8c599486bdd92eba0550d
Ridhima Mishra <100011986+mishri10@users.noreply.github.com>
2025-07-02 14:58:52 +0530

    Time: 6 ms (21.17%), Space: 19.9 MB (12.29%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,45 +1,37 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        //if min hea- is used the T.C: O(Nlogk) as there will be atmost k elements           in the heap
-        
-        
-        //bucket sort approch
-        //will make an array having size n+1 for covering all the case even when each element is unique
-        
-        //the bucket[i]=element where the element has freq i;
-        
-        
-        
+        // N^2 + O(K) --> Brute force
+        // unordered_map, priority_queue O(N)+O(NlogN) --> better
+        // unordered_map, bucket where index represent freq O(N)+ O(N) + O(K) bucket sort --> optimal
+
         unordered_map<int,int>mp;
+
+        for(auto i: nums)
+            mp[i]++;
+            
         int n=nums.size();
-        vector<vector<int>>bucket(n+1);
+
+        vector<vector<int>>freq(n+1);
+
+        for(auto i: mp)
+            freq[i.second].push_back(i.first);
+
         vector<int>ans;
-        
-        for(auto it:nums){
-            mp[it]++;
-        }
-        
-        for(auto it:mp){
-            int freq=it.second;
-            int val=it.first;
-            bucket[freq].push_back(val);
-        }
-        
-        
-        for(int i=n;i>=0;--i){
-            
-           
-            
-            while(bucket[i].size()>0 and k>0){
-                ans.push_back(bucket[i].back());
-                bucket[i].pop_back();
-                k--;
+
+        for(int i= n; i>=0; i--){
+            if(freq[i].size()!=0){
+                int j=0;
+
+                while(j<freq[i].size() and k>0){
+                    ans.push_back(freq[i][j]);
+                    j++;
+                    k--;
+                }
             }
+
         }
-        
+
         return ans;
-        
-        
     }
 };
