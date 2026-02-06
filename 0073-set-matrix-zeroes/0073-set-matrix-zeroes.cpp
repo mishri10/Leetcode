43797539e998422bc7bdb412973ab8d7c8cdd5e5
Ridhima Mishra <100011986+mishri10@users.noreply.github.com>
2026-02-06 20:08:47 +0530

    Time: 4 ms (5.37%), Space: 18.7 MB (51.75%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,55 +1,78 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
+        bool setFirstRowZero=0;
+        bool setFirstColZero=0;
+        int m=matrix.size();
+        int n=matrix[0].size();
 
-       int n=matrix.size();
-       int m=matrix[0].size();
+        for(int i=0; i<n; i++){
+            if(matrix[0][i]==0)
+                setFirstRowZero=1;
+        }
 
-       bool Row0=0, Col0=0;  //1,1
+        for(int row=0; row<m; row++){
+            if(matrix[row][0]==0)
+                setFirstColZero=1;
+        }
 
-       for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
-            if(matrix[i][j]==0){
-                if(i==0)
-                    Row0=1;
-                if(j==0)
-                    Col0=1;
-                else{
-                    matrix[0][j]=0;
-                    matrix[i][0]=0;
+        for(int row=1;row<m;row++){
+            for(int col=1; col<n; col++){
+                if(matrix[row][col]==0){
+                    matrix[row][0]=0;
+                    matrix[0][col]=0;
                 }
             }
         }
-       }
 
-       for(int row=1; row<n; row++){
-        if(matrix[row][0]==0){
-            for(int col=1; col<m; col++){
-                matrix[row][col]=0;
+        for(int col=1; col<n; col++){
+            if(matrix[0][col]==0){
+                for(int row=1;row<m; row++){
+                    matrix[row][col]=0;
+                }
             }
         }
-       }
 
-       for(int col=1; col<m; col++){
-        if(matrix[0][col]==0){
-            for(int row=1; row<n; row++){
-                matrix[row][col]=0;
+        for(int row=1; row<m; row++){
+            if(matrix[row][0]==0){
+                for(int col=1;col<n; col++){
+                    matrix[row][col]=0;
+                }
             }
         }
-       }
 
-       if(Row0==1){
-        for(int col=0; col<m; col++){
-            matrix[0][col]=0;
+        if(setFirstRowZero==1){
+            for(int col=0; col<n; col++){
+                matrix[0][col]=0;
+            }
         }
-       }
 
-       if(Col0==1){
-        for(int row=0; row<n; row++){
-            matrix[row][0]=0;
+        if(setFirstColZero==1){
+            for(int row=0; row<m; row++){
+                matrix[row][0]=0;
+            }
         }
-       }
 
-       
+
+
+
+
+
+
+
+
+
     }
 };
+
+
+// [[0,1,2,0],
+//  [3,4,5,2],
+//  [1,3,1,5]]
+
+// T.C: O(m*n)
+// S.C: O(m+n)
+
+
+// T.C: O(n+m) + O(n*m)
+// S.C: O(2) ~ constant
